e3780ce5eec1f5eae84346a959b99ef498207fda
Samuel <a_samuel.calderon.gonzalez@iespablopicasso.es>
2021-11-25 13:09:08 +0100

    Busca los numeros iguales en el array

--- array.java
+++ array.java
@@ -6,13 +6,20 @@ public class array {
 		int[] array = new int[20];
 		System.out.print("[");
 		for (int i = 0; i < array.length; i++) {
-			array[i] =(int) ((Math.random()*10)+1);
-			System.out.print("  "+ array[i]);
+			array[i] = (int) ((Math.random() * 10) + 1);
+			System.out.print("  " + array[i]);
 		}
 		System.out.print("  ]");
-		
+
 		for (int i = 0; i < array.length; i++) {
-			System.out.print("  "+array[i]);
+			System.out.print("  " + array[i]);
+		}
+		int[] contadores = new int [array.lenght];
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array.length; j++) {
+				if(array[i] == array[j])
+					contador[i]++;
+			}
 		}
 	}
 
