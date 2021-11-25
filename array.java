6cc30ddbb8a86fe50e4f4d4d10650aa330882cc5
Samuel <a_samuel.calderon.gonzalez@iespablopicasso.es>
2021-11-25 13:22:40 +0100

    30 numeros

--- array.java
+++ array.java
@@ -3,7 +3,7 @@ package EEDD;
 public class array {
 
 	public static void main(String[] args) {
-		int[] array = new int[20];
+		int[] array = new int[30];
 		System.out.print("[");
 		for (int i = 0; i < array.length; i++) {
 			array[i] =(int) ((Math.random()*10)+1);
